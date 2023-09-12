53d310c720158a98668aa8b686a5eb08a1b0a283
Shakibul Alam <droid@Shakibuls-Air.station>
2023-09-12 21:35:33 +0100

    applied useState,useEffect, fetch json data,use props, create multi component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
   
 
   return (
     <>
-     <h2>Rest Countries</h2>
+     <h1>React World Tour</h1>
+    <Countries></Countries>
     </>
   )
 }
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,11 @@
+const Country = ({country}) => {
+const {name} = country;
+    return (
+        <div>
+            <h3>Country Name{country.capital}</h3>
+           
+        </div>
+    );
+};
+
+export default Country;
