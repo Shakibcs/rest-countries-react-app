fc1cafb1188be8fdcfdc4e3acf9bb0e902b420c5
Shakibul Alam <droid@Shakibuls-Air.station>
2023-09-12 22:51:11 +0100

    visited not visited option with state change

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../country/country.jsx";
+import './Countries.css'
 const Countries = () => {
     const [countries,setCountries] = useState([]);
 
@@ -11,11 +12,11 @@ const Countries = () => {
     return (
         <div>
             <h3>Coutries:{countries.length}</h3>
-
+<div className="contries-container">
             {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
             }
-            
+            </div>
         </div>
 
         
